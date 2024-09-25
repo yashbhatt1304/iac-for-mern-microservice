a83bc01515d701d0469fc0018f57fb15e3cabb46
Yash_Bhatt <yashbhatt1304@gmail.com>
2024-09-26 01:58:37 +0530

    Adding https inbound rule in security group

--- main.tf
+++ main.tf
@@ -85,6 +85,13 @@ resource "aws_security_group" "public_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  
+  ingress {
+    from_port   = 443   # Allow HTTP traffic
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port   = 0
