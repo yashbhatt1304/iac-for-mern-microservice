281bc8f88058fcc8b1c142ec63facde282ee2099
Yash_Bhatt <yashbhatt1304@gmail.com>
2024-09-26 01:51:32 +0530

    Adding public and private subnet

--- main.tf
+++ main.tf
@@ -19,3 +19,28 @@ resource "aws_vpc" "my_vpc" {
     Application = var.mern_application_tag 
   }
 }
+
+#######  Public Subnet in Availability Zone 1  ########
+resource "aws_subnet" "public_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "ap-northeast-2a"  # Choose your preferred AZ
+
+  tags = {
+    Name = "PublicSubnet"
+    Application = var.mern_application_tag 
+  }
+}
+
+#######  Private Subnet in Availability Zone 1  ########
+resource "aws_subnet" "private_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "ap-northeast-2a"
+
+  tags = {
+    Name = "PrivateSubnet"
+    Application = var.mern_application_tag 
+  }
+}
+
--- output.tf
+++ output.tf
@@ -2,3 +2,13 @@ output "vpc_id" {
   description = "This is VPC ID"
   value = aws_vpc.my_vpc.id
 }
+
+output "public_subnet_id" {
+  description = "This is public subnet id"
+  value = aws_subnet.public_subnet.id
+}
+
+output "private_subnet_id" {
+  description = "This is private subnet id"
+  value = aws_subnet.private_subnet.id
+}
